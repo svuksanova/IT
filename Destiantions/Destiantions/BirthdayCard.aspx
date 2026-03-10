57d1c4294fb7966059e0ff07de25b1e8db4e8a28
Stefani Vuksanova <46635188+svuksanova@users.noreply.github.com>
2026-03-10 17:37:58 +0100

    Birthday card exercise

---
+++ Destiantions/Destiantions/BirthdayCard.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="BirthdayCard.aspx.cs" Inherits="Destiantions.BirthdayCard" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+
+                <div class="m-4">
+                    Изберете боја за позадина:
+                    <br />
+                    <asp:DropDownList ID="BackgroundColor" runat="server"></asp:DropDownList>
+                </div>
+
+                <div class="m-4">
+                    Изберете фонт:
+                    <br />
+                    <asp:DropDownList ID="Font" runat="server"></asp:DropDownList>
+                </div>
+
+                <div class="m-4">
+                    Изберете боја на фонт:
+                    <br />
+                    <asp:DropDownList ID="FontColor" runat="server"></asp:DropDownList>
+                </div>
+
+                <div class="m-4">
+                    Изберете големина на фонт:
+                    <br />
+                    <asp:TextBox ID="FontSize" runat="server"></asp:TextBox>
+                </div>
+
+                <div class="m-4">
+                    Тип на рамка:
+                    <br />
+                    <asp:RadioButtonList ID="BorderType" runat="server"></asp:RadioButtonList>
+                </div>
+
+                <div class="m-4">
+                    Додади слика:
+                    <br />
+                    <asp:CheckBox ID="ShowImage" runat="server" />
+                </div>
+
+                <div class="m-4">
+                    Внесете ја содржината на честитката:
+                    <br />
+                    <asp:TextBox ID="CardContent" runat="server" TextMode="MultiLine"></asp:TextBox>
+                </div>
+
+                <div class="m-4">
+                    <asp:Button ID="Save" runat="server" Text="Креирај" OnClick="Save_Click" />
+                </div>
+            </div>
+
+            <div class="col-5">
+                <asp:Panel ID="Card" runat="server">
+                    <asp:Label ID="Poraka" runat="server" Text="Label"></asp:Label>
+                    <asp:Image ID="Image1" runat="server" ImageUrl="~/birthdayImg.jpg" Visible="False" />
+                </asp:Panel>
+            </div>
+        </div>
+</div>
+
+</asp:Content>
---
+++ Destiantions/Destiantions/BirthdayCard.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Destiantions
+{
+    public partial class BirthdayCard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BackgroundColor.Items.Add(new ListItem("Green"));
+            BackgroundColor.Items.Add(new ListItem("Yellow"));
+            BackgroundColor.Items.Add(new ListItem("Red"));
+            BackgroundColor.Items.Add(new ListItem("Blue"));
+            BackgroundColor.Items.Add(new ListItem("Pink"));
+            BackgroundColor.Items.Add(new ListItem("Gray"));
+            BackgroundColor.Items.Add(new ListItem("Orange"));
+            BackgroundColor.Items.Add(new ListItem("Purple"));
+
+            Font.Items.Add(new ListItem("Arial"));
+            Font.Items.Add(new ListItem("Times New Roman"));
+            Font.Items.Add(new ListItem("Verdana"));
+            Font.Items.Add(new ListItem("Courier"));
+
+            FontColor.Items.Add(new ListItem("Green"));
+            FontColor.Items.Add(new ListItem("Yellow"));
+            FontColor.Items.Add(new ListItem("Red"));
+            FontColor.Items.Add(new ListItem("Blue"));
+            FontColor.Items.Add(new ListItem("Pink"));
+            FontColor.Items.Add(new ListItem("Gray"));
+            FontColor.Items.Add(new ListItem("Orange"));
+            FontColor.Items.Add(new ListItem("Purple"));
+
+            BorderType.Items.Add(new ListItem(BorderStyle.None.ToString(), ((int)BorderStyle.None).ToString()));
+            BorderType.Items.Add(new ListItem(BorderStyle.Solid.ToString(), ((int)BorderStyle.Solid).ToString()));
+            BorderType.Items.Add(new ListItem(BorderStyle.Ridge.ToString(), ((int)BorderStyle.Ridge).ToString()));
+            BorderType.Items.Add(new ListItem(BorderStyle.Groove.ToString(), ((int)BorderStyle.Groove).ToString()));
+        }
+
+        protected void Save_Click(object sender, EventArgs e)
+        {
+            Poraka.Text = CardContent.Text;
+            Poraka.Font.Name = Font.SelectedItem.Text;
+
+            int size;
+            var success = Int32.TryParse(FontSize.Text, out size);
+
+            if (success)
+            {
+                Poraka.Font.Size = size;
+            }
+
+            if (ShowImage.Checked)
+            {
+                Image1.Visible = true;
+            }
+
+            Card.BackColor = Color.FromName(BackgroundColor.SelectedItem.Text);
+
+        }
+    }
+}
